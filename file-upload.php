7ea43de0bd27b0df38423a6176ff6ff0ae405d04
Koustubh <koustub28@hotmail.com>
2021-12-22 19:30:03 +0530

    file upload php

---
+++ file-upload.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_FILES['image'])) {
+//    echo "<pre>";
+//    print_r($_FILES);
+//    echo "</pre>";
+
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+
+    move_uploaded_file($file_tmp, "upload-image/". $file_name);
+}
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload</title>
+</head>
+<body>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="image" /><br><br>
+        <input type="submit" />
+    </form>
+</body>
+</html>
